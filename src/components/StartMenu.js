2a2e33c590cb5273c3d78e5cb2c165e126cdbdd4
Rafay <inshalnadeem01@gmail.com>
2024-11-20 20:56:35 +0500

    start page responsived

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -12,19 +12,20 @@ const StartMenu = ({ onStartGame }) => {
             className="flex flex-col items-center justify-center h-screen w-full bg-cover bg-center"
             style={{ backgroundImage: "url('/images/bg-snake-game.png')" }}
         >
-            <h1 className="text-white text-5xl font-extrabold mb-4 animate-bounce">Snake Game</h1>
-            <p className="text-xl font-semibold mb-8 animate-pulse">Use the arrow keys or WASD to move the snake</p>
-            <button
-                className="px-6 py-3 text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
+            <h1 className="text-white text-4xl sm:text-5xl font-bold mb-4 animate-bounce">Snake Game</h1>
+            <p className="text-lg xxs:text-center xxs:text-sm text-white sm:text-xl font-semibold mb-8 animate-pulse">Use the arrow keys or WASD to move the snake</p>
 
+            <button
+                className="text-sm xxs:px-4 xxs:py-2 sm:px-6 sm:py-3 sm:text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
                 onClick={handleStartClick}
-            >   
+            >
                 Start Game
             </button>
+
             <select
                 value={difficulty}
                 onChange={(e) => setDifficulty(e.target.value)}
-                className="mt-4 px-6 py-3 text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
+                className="mt-4 px-6 py-3 text-sm xxs:px-3 xxs:py-1 sm:px-6 sm:py-3  sm:text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
             >
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'xxs': '320px',
+        'xs': '480px',   // Custom 'xs' breakpoint
+        'sm': '640px',   // Default 'sm' breakpoint
+        'md': '768px',   // Default 'md' breakpoint
+        'lg': '1024px',  // Default 'lg' breakpoint
+        'xl': '1280px',  // Default 'xl' breakpoint
+        '2xl': '1536px', // Default '2xl' breakpoint
+      },
     },
   },
   plugins: [],
