60da325c8b3ea7c297dd8eb951a6fd7869d7d00b
Rafay <inshalnadeem01@gmail.com>
2024-11-22 00:27:42 +0500

    changed start game button color

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -18,7 +18,7 @@ const StartMenu = ({ onStartGame }) => {
             <p className="text-lg 3xs:text-center 3xs:text-sm text-white sm:text-xl font-semibold mb-8 animate-pulse">Use the arrow keys or WASD to move the snake</p>
 
             <button
-                className="text-sm 3xs:px-4 3xs:py-2 sm:px-6 sm:py-3 sm:text-xl text-white bg-blue-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
+                className="text-sm 3xs:px-4 3xs:py-2 sm:px-6 sm:py-3 sm:text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
                 onClick={handleStartClick}
             >
                 Start Game  
@@ -27,7 +27,7 @@ const StartMenu = ({ onStartGame }) => {
             <select
                 value={difficulty}
                 onChange={(e) => setDifficulty(e.target.value)}
-                className="mt-4 text-sm 3xs:px-4 3xs:py-2 sm:px-6 sm:py-3  sm:text-xl text-white bg-yellow-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
+                className="mt-4 text-sm 3xs:px-4 3xs:py-2 sm:px-6 sm:py-3  sm:text-xl text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 transition duration-300"
             >
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
